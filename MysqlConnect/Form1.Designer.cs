5fc1034a6785e79d99ecfeb93bf35bea2b7cc47b
kurs <kurs@KURS-KOMPUTER>
2020-09-13 15:18:40 +0200

    update

---
+++ MysqlConnect/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace MysqlConnect
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbHost = new System.Windows.Forms.TextBox();
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbPass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbDbName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.tbSQL = new System.Windows.Forms.TextBox();
+            this.btnRunSQL = new System.Windows.Forms.Button();
+            this.lvGrid = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Host";
+            // 
+            // tbHost
+            // 
+            this.tbHost.Location = new System.Drawing.Point(95, 19);
+            this.tbHost.Name = "tbHost";
+            this.tbHost.Size = new System.Drawing.Size(141, 20);
+            this.tbHost.TabIndex = 1;
+            this.tbHost.Text = "127.0.0.1";
+            // 
+            // tbUser
+            // 
+            this.tbUser.Location = new System.Drawing.Point(95, 62);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(141, 20);
+            this.tbUser.TabIndex = 3;
+            this.tbUser.Text = "root";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "User";
+            // 
+            // tbPass
+            // 
+            this.tbPass.Location = new System.Drawing.Point(95, 107);
+            this.tbPass.Name = "tbPass";
+            this.tbPass.Size = new System.Drawing.Size(141, 20);
+            this.tbPass.TabIndex = 5;
+            this.tbPass.Text = "root";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password";
+            // 
+            // tbDbName
+            // 
+            this.tbDbName.Location = new System.Drawing.Point(95, 147);
+            this.tbDbName.Name = "tbDbName";
+            this.tbDbName.Size = new System.Drawing.Size(141, 20);
+            this.tbDbName.TabIndex = 7;
+            this.tbDbName.Text = "classicmodels";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "DB Name";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(63, 200);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(111, 47);
+            this.btnConnect.TabIndex = 8;
+            this.btnConnect.Text = "Połącz z bazą";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // tbSQL
+            // 
+            this.tbSQL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tbSQL.Location = new System.Drawing.Point(263, 14);
+            this.tbSQL.Multiline = true;
+            this.tbSQL.Name = "tbSQL";
+            this.tbSQL.Size = new System.Drawing.Size(498, 149);
+            this.tbSQL.TabIndex = 9;
+            this.tbSQL.Text = "SELECT * FROM customers WHERE \r\ncustomerNumber>=200 AND customerNumber<=300 \r\nAND" +
+    " city LIKE \"C%\"\r\nORDER BY customerNumber DESC, contactFirstName ASC \r\n\r\n\r\n";
+            // 
+            // btnRunSQL
+            // 
+            this.btnRunSQL.Location = new System.Drawing.Point(795, 65);
+            this.btnRunSQL.Name = "btnRunSQL";
+            this.btnRunSQL.Size = new System.Drawing.Size(111, 47);
+            this.btnRunSQL.TabIndex = 10;
+            this.btnRunSQL.Text = "Wykonaj SQL";
+            this.btnRunSQL.UseVisualStyleBackColor = true;
+            // 
+            // lvGrid
+            // 
+            this.lvGrid.Location = new System.Drawing.Point(263, 186);
+            this.lvGrid.Name = "lvGrid";
+            this.lvGrid.Size = new System.Drawing.Size(663, 334);
+            this.lvGrid.TabIndex = 11;
+            this.lvGrid.UseCompatibleStateImageBehavior = false;
+            this.lvGrid.View = System.Windows.Forms.View.Details;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(959, 532);
+            this.Controls.Add(this.lvGrid);
+            this.Controls.Add(this.btnRunSQL);
+            this.Controls.Add(this.tbSQL);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.tbDbName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbPass);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbUser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbHost);
+            this.Controls.Add(this.label1);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbHost;
+        private System.Windows.Forms.TextBox tbUser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbPass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbDbName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox tbSQL;
+        private System.Windows.Forms.Button btnRunSQL;
+        private System.Windows.Forms.ListView lvGrid;
+    }
+}
+
---
+++ MysqlConnect/Form1.cs
@@ -0,0 +1,50 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MysqlConnect
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        String connectionString = "";
+        bool isOpen = false;
+        MySqlConnection conn = null;
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            connectionString = $"server={tbHost.Text};userid={tbUser.Text};password={tbPass.Text};database={tbDbName.Text};port=3306";
+            try
+            {
+                if (isOpen)
+                {
+                    // rozłącz
+                    conn.Close();
+                    conn.Dispose();
+                    isOpen = false;
+                    btnConnect.Text = "Połącz";
+                } else
+                {
+                    // połącz
+                    conn = new MySqlConnection(connectionString);
+                    conn.Open();
+                    isOpen = true;
+                    btnConnect.Text = "Rozłącz";
+                }
+            } catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+    }
+}
